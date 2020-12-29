807198348e3fd4817cd0a665b985d19659b35e7a
Josh <thorne.josh@outlook.com>
2020-12-29 15:50:39 +0000

    Poor commented and structured LinearClassifier.

--- OpenIMAJ-CW3/src/main/java/uk/ac/soton/ecs/cw3/LinearClassifier.java
+++ OpenIMAJ-CW3/src/main/java/uk/ac/soton/ecs/cw3/LinearClassifier.java
@@ -3,14 +3,25 @@ package uk.ac.soton.ecs.cw3;
 import de.bwaldvogel.liblinear.SolverType;
 import org.apache.commons.vfs2.FileSystemException;
 import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
-import org.openimaj.feature.FeatureExtractor;
-import org.openimaj.feature.SparseIntFV;
+import org.openimaj.experiment.dataset.sampling.GroupSampler;
+import org.openimaj.experiment.evaluation.classification.ClassificationEvaluator;
+import org.openimaj.experiment.evaluation.classification.ClassificationResult;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMAnalyser;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMResult;
+import org.openimaj.feature.*;
+import org.openimaj.feature.local.LocalFeature;
+import org.openimaj.feature.local.LocalFeatureImpl;
+import org.openimaj.feature.local.SpatialLocation;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
 import org.openimaj.image.feature.local.aggregate.BlockSpatialAggregator;
+import org.openimaj.image.pixel.sampling.RectangleSampler;
+import org.openimaj.image.processing.algorithm.MeanCenter;
+import org.openimaj.math.geometry.shape.Rectangle;
 import org.openimaj.ml.annotation.linear.LiblinearAnnotator;
 import org.openimaj.ml.clustering.FloatCentroidsResult;
 import org.openimaj.ml.clustering.assignment.HardAssigner;
@@ -54,13 +65,12 @@ public class LinearClassifier {
         //Instantiate patch extractor for densely-sampled pixel patches
         //TODO: Write up patch extractor method
         PatchExtractor patchExtractor = new PatchExtractor();
-
         //Instantiate hard assigner
         //TODO: Parse the trainingDataset of correct type
-        HardAssigner<float[], float[], IntFloatPair> assigner = trainQuantiser(trainingDataset, patchExtractor);
+        HardAssigner<float[], float[], IntFloatPair> assigner = trainQuantiser(trainingDataset,patchExtractor);
 
         //Use Bag of Visual Words Extractor (no need for SIFT features as shown in Chapter 12)
-        FeatureExtractor<SparseIntFV, FImage> extractor = new BOVWExtractor(patchExtractor, assigner);
+        FeatureExtractor<DoubleFV, FImage> extractor = new BOVWExtractor(patchExtractor,assigner);
 
         //Use LiblinearAnnotator to construct and train classifier
         LiblinearAnnotator<FImage, String> annotator = new LiblinearAnnotator<>(
@@ -68,20 +78,82 @@ public class LinearClassifier {
 
         //Train the dataset using the openimaj's annotator
         annotator.train(trainingDataset);
+
+        ClassificationEvaluator<CMResult<String>, String, FImage> eval =
+                new ClassificationEvaluator<CMResult<String>, String, FImage>(
+                        annotator, trainingDataset, new CMAnalyser<FImage, String>(CMAnalyser.Strategy.SINGLE));
+
+        Map<FImage, ClassificationResult<String>> guesses = eval.evaluate();
+        CMResult<String> result = eval.analyse(guesses);
+
+        System.out.println(result.getDetailReport());
+
+    }
+
+    /**
+     * Base code adapted from HardAssigner class provided in Chapter 12
+     * Not too sure about the exact methods and data types of this
+     * <p>
+     * TODO: To write hard assigner that can assign features (from patch extractor) to identifiers
+     */
+    public static HardAssigner<float[], float[], IntFloatPair> trainQuantiser(GroupedDataset<String, VFSListDataset<FImage>, FImage> trainingDataset, PatchExtractor extractor) {
+
+        //Some sort of list here to hold in features, as per Chap 12?
+        List<float[]> allkeys = new ArrayList<float[]>();
+
+        //Iterate through training dataset (utilising Josh's GroupedDataset method)
+        for (FImage image : trainingDataset) {
+            List<LocalFeature<SpatialLocation, FloatFV>> sampleList = extractor.extract(image, 4, 8);
+
+            for (LocalFeature<SpatialLocation, FloatFV> localFeature : sampleList) {
+                allkeys.add(localFeature.getFeatureVector().values);
+            }
+        }
+
+        //A sample of these should be clustered using K-Means to learn a vocabulary (try ~500 clusters to start).
+        FloatKMeans km = FloatKMeans.createKDTreeEnsemble(500);
+        float[][] datasource = allkeys.toArray(new float[][]{});
+
+        FloatCentroidsResult result = km.cluster(datasource);
+        return result.defaultHardAssigner();
+    }
+
+    public static class PatchExtractor {
+
+        public LocalFeature<SpatialLocation, FloatFV> getLocalFeature(FImage image, Rectangle rectangle) {
+            image.processInplace(new MeanCenter());
+            image.normalise();
+            FloatFV feature = new FloatFV(image.getFloatPixelVector());
+            return new LocalFeatureImpl<SpatialLocation, FloatFV>(
+                    new SpatialLocation(rectangle.x, rectangle.y), feature);
+        }
+
+        public List<LocalFeature<SpatialLocation, FloatFV>> extract(FImage image, int step, int size) {
+            RectangleSampler sampler = new RectangleSampler(image, step, step, size, size);
+            List<LocalFeature<SpatialLocation, FloatFV>> features = new ArrayList<LocalFeature<SpatialLocation, FloatFV>>();
+
+            for (Rectangle rectange : sampler) {
+                FImage patch = image.extractROI(rectange);
+                LocalFeature<SpatialLocation, FloatFV> localFeature = getLocalFeature(patch, rectange);
+                features.add(localFeature);
+            }
+
+            return features;
+        }
     }
 
     /**
      * Implements FeatureExtractor class
      * Not too sure on this methods integration with "PatchExtractor", I know somewhere we need to
      * Sample 8x8 patches every 4 pixels (in X and Y direction)
-     *
+     * <p>
      * Adapted from chapter 12's PHOWExtractor Implementation:
      * http://openimaj.org/tutorial/classification101.html
      *
      * @implements FeatureExtractor interface http://openimaj.org/apidocs/org/openimaj/feature/FeatureExtractor.html
      * @implements BagOfVisualWords: http://openimaj.org/apidocs/org/openimaj/image/feature/local/aggregate/BagOfVisualWords.html
      */
-    public static class BOVWExtractor implements FeatureExtractor<SparseIntFV, FImage> {
+    public static class BOVWExtractor implements FeatureExtractor<DoubleFV, FImage> {
         PatchExtractor patchExtractor;
         HardAssigner<float[], float[], IntFloatPair> assigner;
 
@@ -92,56 +164,20 @@ public class LinearClassifier {
         }
 
         // Implemented Methods
-        @Override
-        public SparseIntFV extractFeature(FImage image) {
-
+        public DoubleFV extractFeature(FImage image) {
             //Extracts basic (hard-assignment) bag of visual words (BOVW) representations of an image given a list of
             //local features and an HardAssigner
             BagOfVisualWords<float[]> bovw = new BagOfVisualWords<float[]>(assigner);
 
             //Use BlockSpatialAggregator together with BagOfVisualWords
-            BlockSpatialAggregator<float[], SparseIntFV> spatialAggregator =
+            BlockSpatialAggregator<float[], SparseIntFV> spatial =
                     new BlockSpatialAggregator<float[], SparseIntFV>(bovw, 2, 2);
 
             //public SparseIntFV aggregate(List<? extends LocalFeature<?,? extends ArrayFeatureVector<T>>> features)
-            //TODO: sort out first element of this return type??
-            return spatialAggregator.aggregate(patchExtractor.extract(image), image.getBounds());
+            return spatial.aggregate(patchExtractor.extract(image, 4, 8), image.getBounds()).normaliseFV();
         }
-    }
-
-    /**
-     * TODO: to write something that can extract 8x8 patches every 4 pixels etc...
-     */
-    public static class PatchExtractor {
-        public FImage extract(FImage image) {
-
-
-            return image;
-        }
-    }
-
-    /**
-     * Base code adapted from HardAssigner class provided in Chapter 12
-     * Not too sure about the exact methods and data types of this
-     *
-     * TODO: To write hard assigner that can assign features (from patch extractor) to identifiers
-     */
-    public static HardAssigner<float[], float[], IntFloatPair> trainQuantiser(VFSGroupDataset<FImage> trainingDataset, PatchExtractor patchExtractor) {
-        //Some sort of list here to hold in features, as per Chap 12?
-        List<float[]> allkeys = new ArrayList<float[]>();
-
-        //Iterate through training dataset (utilising Josh's GroupedDataset method)
-        for (Map.Entry<String, VFSListDataset<FImage>> entry : trainingDataset.entrySet()) {
-            //TODO: Apply Patch/Feature Extractor to each image in the dataset ?
-
-        }
-        //TODO: Possibly consider mean-centring and normalising each patch before clustering/quantisation here?
-
-        //A sample of these should be clustered using K-Means to learn a vocabulary (try ~500 clusters to start).
-        FloatKMeans km = FloatKMeans.createKDTreeEnsemble(500);
-        float[][] datasource = allkeys.toArray(new float[][]{});
-        //DataSource<float[]> datasource = new LocalFeatureListDataSource<>(allkeys);
-        FloatCentroidsResult result = km.cluster(datasource);
-        return result.defaultHardAssigner();
     }
 }
+
+
+
