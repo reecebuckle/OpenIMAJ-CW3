5a2bb36077f123efbe81ce4d8d73c41a686b20b0
Max Morby-Roy <max.m.roy90@gmail.com>
2021-01-05 12:36:49 +0000

    2 neural nets have been set up one uses a pretrained NASNet, the other uses an untrained ResNET50

--- OpenIMAJ-CW3/src/main/java/uk/ac/soton/ecs/cw3/App.java
+++ OpenIMAJ-CW3/src/main/java/uk/ac/soton/ecs/cw3/App.java
@@ -1,6 +1,12 @@
 package uk.ac.soton.ecs.cw3;
 
-import java.io.FileWriter;
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.colour.ColourSpace;
+import org.openimaj.image.colour.RGBColour;
+import org.openimaj.image.processing.convolution.FGaussianConvolve;
+import org.openimaj.image.typography.hershey.HersheyFont;
+
 import java.io.IOException;
 
 /**
@@ -8,70 +14,14 @@ import java.io.IOException;
  *
  */
 public class App {
-
     public static void main( String[] args ) {
 
         try {
-
-            TinyImageKNNClassifier tinyImageKNNClassifier = new TinyImageKNNClassifier(16, 18);
-            tinyImageKNNClassifier.init();
-            tinyImageKNNClassifier.classifyImages("Tiny Image Classifier Results.txt");
-
-//            testTinyImageKNN();
-
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
+            CNN cnn = new CNN();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-
-    }
-
-    /**
-     * Method used for testing the tinyImageKNNClassifier to tune k and or small image size.
-     * @throws IOException Does what it says on the tin
-     */
-    public static void testTinyImageKNN() throws IOException {
-
-        FileWriter fileWriter = new FileWriter("Tiny Image Classifier Tuning.csv");
-        fileWriter.write("K, accuracy, error\n");
-
-        for (int k = 1; k < 50; k++) {
-
-            System.out.println(String.format("Calculating accuracy/error for k-neighbours: %d", k));
-
-            int n = 20;
-            double[] acc_avg = new double[n], err_avg = new double[n];
-
-            for (int i = 0; i < n; i++) {
-                TinyImageKNNClassifier tinyImageKNNClassifier = new TinyImageKNNClassifier(16, k);
-
-                // Load the dataset
-                tinyImageKNNClassifier.setTestTrainSize(80, 20);
-                tinyImageKNNClassifier.splitData();
-
-                tinyImageKNNClassifier.trainClassifier();
-
-                String results = tinyImageKNNClassifier.testClassifier();
-                String accuracy = results.split("\n")[0].split(": ")[1].replaceAll("\\s+", "");
-                String error = results.split("\n")[1].split(": ")[1].replaceAll("\\s+", "");
-
-                acc_avg[i] = Double.parseDouble(accuracy);
-                err_avg[i] = Double.parseDouble(error);
-            }
-
-            double acc_sum = 0;
-            double err_sum = 0;
-            for (int d = 0; d < n; d++) {
-                acc_sum += acc_avg[d];
-                err_sum += err_avg[d];
-            }
-
-            double accuracy = acc_sum / acc_avg.length;
-            double error = err_sum / acc_avg.length;
-
-            fileWriter.write(String.format("%d, %f, %f\n", k, accuracy, error));
-        }
-
-        fileWriter.close();
     }
 }
---
+++ OpenIMAJ-CW3/src/main/java/uk/ac/soton/ecs/cw3/CNN.java
@@ -0,0 +1,241 @@
+package uk.ac.soton.ecs.cw3;
+
+import org.datavec.api.io.filters.BalancedPathFilter;
+import org.datavec.api.io.labels.ParentPathLabelGenerator;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.split.InputSplit;
+import org.datavec.image.loader.BaseImageLoader;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.datavec.image.transform.ImageTransform;
+import org.datavec.image.transform.MultiImageTransform;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.transferlearning.FineTuneConfiguration;
+import org.deeplearning4j.nn.transferlearning.TransferLearning;
+import org.deeplearning4j.optimize.listeners.EvaluativeListener;
+import org.deeplearning4j.optimize.listeners.PerformanceListener;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.optimize.listeners.TimeIterationListener;
+import org.deeplearning4j.util.ModelSerializer;
+import org.deeplearning4j.zoo.PretrainedType;
+import org.deeplearning4j.zoo.ZooModel;
+import org.deeplearning4j.zoo.model.NASNet;
+import org.deeplearning4j.zoo.model.ResNet50;
+import org.nd4j.evaluation.classification.Evaluation;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+
+public class CNN {
+
+    private static final String[] allowedExtensions = BaseImageLoader.ALLOWED_FORMATS; // acceptable image formats
+    private static final long seed = 777; // seed used for randomizing data
+
+    private static final int height = 200;
+    private static final int width = 200;
+    private static final int channels = 3;
+
+    private static final Random randNumGen = new Random(seed);
+
+    private final String baseDir = System.getProperty("user.dir") + "\\OpenIMAJ-CW3\\";
+
+    private final int batchSize = 10;
+    private static final int classes = 15;
+
+    private static final int epochs = 10;
+
+
+    private static DataSetIterator trainIter;
+    private static DataSetIterator testIter;
+
+    private ZooModel nASNet;
+    private ZooModel rESNet50;
+
+    private int lastLayerIndex;
+    
+
+    public CNN () throws IOException, InterruptedException {
+
+
+        setUpTrainTest();
+        rESNet50 = resNETFromScratch(); // run this one please
+//        nASNet = NABSNetPredefined();
+
+    }
+
+    public ZooModel NABSNetPredefined() throws IOException {
+
+
+        ZooModel nASNet = NASNet.builder().build();
+        ComputationGraph net = (ComputationGraph) nASNet.initPretrained(PretrainedType.IMAGENETLARGE);
+
+        lastLayerIndex = net.getLayers().length - 1;
+
+
+        Layer lastLayer = net.getLayers()[lastLayerIndex];
+        String lastLayerName = lastLayer.conf().getLayer().getLayerName();
+
+        Layer featureLayer = net.getLayers()[lastLayerIndex-1];
+        String featureLayerName = featureLayer.conf().getLayer().getLayerName();
+
+        FineTuneConfiguration fineTuneConfiguration = new FineTuneConfiguration.Builder()
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .updater(new Nesterovs(5e-5))
+                .seed(seed)
+                .build();
+
+
+        ComputationGraph nasNetTransfer = new TransferLearning.GraphBuilder(net)
+                .fineTuneConfiguration(fineTuneConfiguration)
+                .setFeatureExtractor(featureLayerName)
+                .addLayer("connector",
+                        new DenseLayer.Builder()
+                            .nIn(1000)
+                            .nOut(256)
+                            .activation(Activation.RELU)
+                            .build(),
+                        lastLayerName)
+                .addLayer("new_predictions",
+                        new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                            .activation(Activation.SOFTMAX)
+                            .nIn(256)
+                            .nOut(classes)
+                            .build(),
+                        "connector")
+                .setOutputs("new_predictions")
+                .build();
+
+
+        nasNetTransfer.setListeners(
+                new ScoreIterationListener(1),
+                new EvaluativeListener(trainIter, 1),
+                new EvaluativeListener(testIter, 1),
+                new PerformanceListener(1),
+                new TimeIterationListener(1));
+
+        double lowest = 10;
+
+        System.out.println("\n --- Training Network ---");
+
+        for (int i = 0; i < epochs; i++) {
+            trainIter.reset();
+            nasNetTransfer.fit(trainIter);
+            System.out.println("epoch: " + i + " score: " + nasNetTransfer.score());
+
+            if (nasNetTransfer.score() < lowest) {
+                lowest = nasNetTransfer.score();
+                System.out.println("epoch: " + i + " lowest: " + lowest);
+                String modelFilename = String.format("%s_lowest_err_%f_epoch_%d_NASNet.zip", baseDir, lowest, i);
+                ModelSerializer.writeModel(nasNetTransfer, modelFilename, false);
+            }
+        }
+
+        ModelSerializer.writeModel(nasNetTransfer, "Final_NASNet.zip", false);
+
+        trainIter.reset();
+        Evaluation trainEval = nasNetTransfer.evaluate(trainIter);
+        Evaluation testEval = nasNetTransfer.evaluate(testIter);
+
+        System.out.println(trainEval.stats());
+        System.out.println(testEval.stats());
+
+        return nASNet;
+    }
+
+    public ZooModel resNETFromScratch() throws IOException {
+
+
+        ZooModel resNet50 = ResNet50.builder()
+                .numClasses(classes)
+                .seed(seed)
+                .inputShape(new int[]{channels, height, width})
+                .updater(new Adam(1e-3))
+                .build();
+
+        ComputationGraph net = resNet50.init();
+
+
+        net.setListeners(
+                new ScoreIterationListener(1),
+                new EvaluativeListener(trainIter, 1),
+                new EvaluativeListener(testIter, 1),
+                new PerformanceListener(1),
+                new TimeIterationListener(1));
+
+        System.out.println("\n --- Training Network ---");
+
+        double lowest = 10;
+
+        for (int i = 0; i < epochs; i++) {
+            trainIter.reset();
+            net.fit(trainIter);
+            System.out.println("epoch: " + i + " score: " + net.score());
+
+            if (net.score() < lowest) {
+                lowest = net.score();
+                System.out.println("epoch: " + i + " lowest: " + lowest);
+                String modelFilename = String.format("%s_lowest_err_%f_epoch_%d_RESNet50.zip", baseDir, lowest, i);
+                ModelSerializer.writeModel(net, modelFilename, false);
+            }
+        }
+
+
+        ModelSerializer.writeModel(net, "Final_RESNet50.zip", false);
+
+        trainIter.reset();
+        Evaluation trainEval = net.evaluate(trainIter);
+        Evaluation testEval = net.evaluate(testIter);
+
+        System.out.println(trainEval.stats());
+        System.out.println(testEval.stats());
+
+        return resNet50;
+    }
+
+    private void setUpTrainTest() throws IOException, InterruptedException {
+
+        File directory = new File(baseDir, "training/");
+        FileSplit fileSplit = new FileSplit(directory, allowedExtensions, randNumGen);
+
+        ParentPathLabelGenerator labelGenerator = new ParentPathLabelGenerator();
+        BalancedPathFilter pathFilter = new BalancedPathFilter(randNumGen, allowedExtensions, labelGenerator);
+
+        InputSplit[] filesInDirSplit = fileSplit.sample(pathFilter, 80, 20);
+        InputSplit trainData = filesInDirSplit[0];
+        InputSplit testData = filesInDirSplit[1];
+
+        ImageRecordReader trainRecordReader = new ImageRecordReader(height, width, channels, labelGenerator);
+        ImageRecordReader testRecordReader = new ImageRecordReader(height, width, channels, labelGenerator);
+
+        // Can add more transforms to augment the data
+        ImageTransform transform = new MultiImageTransform(randNumGen);
+
+        trainRecordReader.initialize(trainData, transform);
+        testRecordReader.initialize(testData, transform);
+
+        trainIter = new RecordReaderDataSetIterator(trainRecordReader, batchSize, 1, classes);
+        testIter = new RecordReaderDataSetIterator(testRecordReader, batchSize, 1, classes);
+
+        ImagePreProcessingScaler normalizer = new ImagePreProcessingScaler();
+
+        trainIter.setPreProcessor(normalizer);
+        testIter.setPreProcessor(normalizer);
+
+    }
+
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
