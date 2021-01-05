ba923a3ed32cc87801bbd23d345615b6105f5edc
Josh <thorne.josh@outlook.com>
2021-01-05 21:42:00 +0000

    Added DenseSIFTClassifier.java.

---
+++ OpenIMAJ-CW3/src/main/java/uk/ac/soton/ecs/cw3/DenseSIFTClassifer.java
@@ -0,0 +1,110 @@
+package uk.ac.soton.ecs.cw3;
+
+import de.bwaldvogel.liblinear.SolverType;
+import org.apache.commons.vfs2.FileSystemException;
+import org.openimaj.data.DataSource;
+import org.openimaj.data.dataset.*;
+import org.openimaj.experiment.dataset.sampling.GroupedUniformRandomisedSampler;
+import org.openimaj.experiment.dataset.split.GroupedRandomSplitter;
+import org.openimaj.experiment.evaluation.classification.ClassificationEvaluator;
+import org.openimaj.experiment.evaluation.classification.ClassificationResult;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMAnalyser;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMResult;
+import org.openimaj.feature.DoubleFV;
+import org.openimaj.feature.FeatureExtractor;
+import org.openimaj.feature.SparseIntFV;
+import org.openimaj.feature.local.data.LocalFeatureListDataSource;
+import org.openimaj.feature.local.list.LocalFeatureList;
+import org.openimaj.image.FImage;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.feature.dense.gradient.dsift.ByteDSIFTKeypoint;
+import org.openimaj.image.feature.dense.gradient.dsift.DenseSIFT;
+import org.openimaj.image.feature.dense.gradient.dsift.PyramidDenseSIFT;
+import org.openimaj.image.annotation.evaluation.datasets.Caltech101;
+import org.openimaj.image.annotation.evaluation.datasets.Caltech101.Record;
+import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
+import org.openimaj.image.feature.local.aggregate.BlockSpatialAggregator;
+import org.openimaj.ml.annotation.Annotated;
+import org.openimaj.ml.annotation.linear.LiblinearAnnotator;
+import org.openimaj.ml.clustering.ByteCentroidsResult;
+import org.openimaj.ml.clustering.assignment.HardAssigner;
+import org.openimaj.ml.clustering.kmeans.ByteKMeans;
+import org.openimaj.util.pair.IntFloatPair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class DenseSIFTClassifer {
+    public static void main(String[] args) throws FileSystemException {
+
+        GroupedDataset<String, VFSListDataset<FImage>, FImage> images =
+                new VFSGroupDataset<>(System.getProperty("user.dir") + "/OpenIMAJ-CW3/training", ImageUtilities.FIMAGE_READER);
+
+        GroupedRandomSplitter<String, FImage> splitter = new GroupedRandomSplitter<String, FImage>(images, 80, 0, 20);
+
+        DenseSIFT dsift = new DenseSIFT(5, 7);
+        PyramidDenseSIFT<FImage> pdsift = new PyramidDenseSIFT<FImage>(dsift, 6f, 7);
+
+        HardAssigner<byte[], float[], IntFloatPair> assigner =
+                trainQuantiser(GroupedUniformRandomisedSampler.sample(splitter.getTrainingDataset(), 30), pdsift);
+
+        FeatureExtractor<DoubleFV, FImage> extractor = new PHOWExtractor(pdsift, assigner);
+
+        LiblinearAnnotator<FImage, String> ann = new LiblinearAnnotator<FImage, String>(
+                extractor, LiblinearAnnotator.Mode.MULTICLASS, SolverType.L2R_L2LOSS_SVC, 1.0, 0.00001);
+
+        ann.train(splitter.getTrainingDataset());
+
+        ClassificationEvaluator<CMResult<String>, String, FImage> eval =
+                new ClassificationEvaluator<CMResult<String>, String, FImage>(
+                        ann, splitter.getTestDataset(), new CMAnalyser<FImage,String>(CMAnalyser.Strategy.SINGLE));
+
+        Map<FImage, ClassificationResult<String>> guesses = eval.evaluate();
+
+        CMResult<String> result = eval.analyse(guesses);
+    }
+
+    public static HardAssigner<byte[], float[], IntFloatPair> trainQuantiser(
+            GroupedDataset<String, ListDataset<FImage>,FImage> sample, PyramidDenseSIFT<FImage> pdsift) {
+        List<LocalFeatureList<ByteDSIFTKeypoint>> allkeys = new ArrayList<LocalFeatureList<ByteDSIFTKeypoint>>();
+
+        for (FImage img : sample) {
+            pdsift.analyseImage(img);
+            allkeys.add(pdsift.getByteKeypoints(0.005f));
+        }
+
+        if (allkeys.size() > 10000)
+            allkeys = allkeys.subList(0, 10000);
+
+        ByteKMeans km = ByteKMeans.createKDTreeEnsemble(300);
+        DataSource<byte[]> datasource = new LocalFeatureListDataSource<ByteDSIFTKeypoint, byte[]>(allkeys);
+        ByteCentroidsResult result = km.cluster(datasource);
+
+        return result.defaultHardAssigner();
+    }
+
+    public static class PHOWExtractor implements FeatureExtractor<DoubleFV, FImage> {
+        PyramidDenseSIFT<FImage> pdsift;
+        HardAssigner<byte[], float[], IntFloatPair> assigner;
+
+        public PHOWExtractor(PyramidDenseSIFT<FImage> pdsift, HardAssigner<byte[], float[], IntFloatPair> assigner) {
+            this.pdsift = pdsift;
+            this.assigner = assigner;
+        }
+
+        @Override
+        public DoubleFV extractFeature(FImage object) {
+            FImage image = object.getImage();
+            pdsift.analyseImage(image);
+
+            BagOfVisualWords<byte[]> bovw = new BagOfVisualWords<byte[]>(assigner);
+
+            BlockSpatialAggregator<byte[], SparseIntFV> spatial = new BlockSpatialAggregator<byte[], SparseIntFV>(
+                    bovw, 2, 2);
+
+            return spatial.aggregate(pdsift.getByteKeypoints(0.015f), image.getBounds()).normaliseFV();
+        }
+    }
+
+}
